f604d164dc1239e503edf8d45520727bc9061e68
Siddhantwork099 <siddhantwork867@gmail.com>
2024-08-27 00:17:33 +0530

    datatype

--- besic_js/index.js
+++ besic_js/index.js
@@ -1,24 +1,24 @@
 console.log("hello word");
 
 // variable>>>>>>
-var a = 40 ; 
+var a = 40;
 let b = 30;
-const c = 40 ;
-// console.log(a);
-// console.log(b);
-// console.log(c);
+const c = 40;
+console.log(a);
+console.log(b);
+console.log(c);
 
 
-// var a = 55;
-// console.log(a);
+var a = 55;
+console.log(a);
 
-//  b =234;
+ b =234;
 
-//  console.log(b);
+ console.log(b);
 
-//  c = 354;
-//  console.log(c);
- 
+ c = 354;
+ console.log(c);
+
 // opretors
 // arthmetic opener. 
 // + , - * /
@@ -28,5 +28,57 @@ console.log(a);
 console.log(a-b);
 console.log(a*b);
 console.log(a/b);
+
+// aasigment opretor 
+a = b;
+// comprerssion opretors
+// == ,> ,< , >= , <= ,!=
+var sid = "24";
+var aman = 24;
+console.log(sid==aman);
+console.log(sid>=aman);
+console.log(sid<=aman);
+console.log(sid>aman);
+console.log(sid<aman);
+console.log(sid!=aman);
+console.log(sid === aman);
+
+console.log(typeof (sid))
+console.log(typeof (aman))
+// data type in javascript 
+// number 1243
+
+// stering  "sid" "24"
+
+//  Boolean true false
+
+// array[1, 3, 5, 6, 3, sid]
+
+// BigInt 2435445463465466547547
+
+// Symbol  unique values
+
+// null  empty
+// undefined not defined 
+console.log(typeof(Number));
+console.log(typeof(NaN));
+console.log(typeof(undefined));
+console.log(typeof(Boolean));
+console.log(typeof(String));
+
+// increment or decrement opretors 
+// ++ ,--
+var a = 30;
+// a++; 
+ a= a+1
+console.log(a);
+a--;
+a= a-1;
+console.log(a);
+
+
+
+
+
 
 
