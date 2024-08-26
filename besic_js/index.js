cd7f2974d325e4841f6cec17d15839e5ac82a301
Siddhantwork099 <siddhantwork867@gmail.com>
2024-08-26 15:32:56 +0530

    class 1

---
+++ besic_js/index.js
@@ -0,0 +1,32 @@
+console.log("hello word");
+
+// variable>>>>>>
+var a = 40 ; 
+let b = 30;
+const c = 40 ;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// var a = 55;
+// console.log(a);
+
+//  b =234;
+
+//  console.log(b);
+
+//  c = 354;
+//  console.log(c);
+ 
+// opretors
+// arthmetic opener. 
+// + , - * /
+console.log(a+b);
+a=b;
+console.log(a);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+
