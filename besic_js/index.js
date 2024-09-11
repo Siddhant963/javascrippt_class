408ae97b594bad4275696d4258d451acfcb54727
Siddhantwork099 <siddhantwork867@gmail.com>
2024-09-11 14:21:19 +0530

    object

--- besic_js/index.js
+++ besic_js/index.js
@@ -1,84 +1,162 @@
-console.log("hello word");
+// console.log("hello word");
 
-// variable>>>>>>
-var a = 40;
-let b = 30;
-const c = 40;
-console.log(a);
-console.log(b);
-console.log(c);
-
-
-var a = 55;
-console.log(a);
-
- b =234;
-
- console.log(b);
-
- c = 354;
- console.log(c);
-
-// opretors
-// arthmetic opener. 
-// + , - * /
-console.log(a+b);
-a=b;
-console.log(a);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-
-// aasigment opretor 
-a = b;
-// comprerssion opretors
-// == ,> ,< , >= , <= ,!=
-var sid = "24";
-var aman = 24;
-console.log(sid==aman);
-console.log(sid>=aman);
-console.log(sid<=aman);
-console.log(sid>aman);
-console.log(sid<aman);
-console.log(sid!=aman);
-console.log(sid === aman);
-
-console.log(typeof (sid))
-console.log(typeof (aman))
-// data type in javascript 
-// number 1243
-
-// stering  "sid" "24"
-
-//  Boolean true false
-
-// array[1, 3, 5, 6, 3, sid]
-
-// BigInt 2435445463465466547547
-
-// Symbol  unique values
-
-// null  empty
-// undefined not defined 
-console.log(typeof(Number));
-console.log(typeof(NaN));
-console.log(typeof(undefined));
-console.log(typeof(Boolean));
-console.log(typeof(String));
-
-// increment or decrement opretors 
-// ++ ,--
-var a = 30;
-// a++; 
- a= a+1
-console.log(a);
-a--;
-a= a-1;
-console.log(a);
+// // variable>>>>>>
+// var a = 40;
+// let b = 30;
+// const c = 40;
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 
+// var a = 55;
+// console.log(a);
+
+//  b =234;
+
+//  console.log(b);
+
+//  c = 354;
+//  console.log(c);
+
+// // opretors
+// // arthmetic opener. 
+// // + , - * /
+// console.log(a+b);
+// a=b;
+// console.log(a);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// // aasigment opretor 
+// a = b;
+// // comprerssion opretors
+// // == ,> ,< , >= , <= ,!=
+// var sid = "24";
+// var aman = 24;
+// console.log(sid==aman);
+// console.log(sid>=aman);
+// console.log(sid<=aman);
+// console.log(sid>aman);
+// console.log(sid<aman);
+// console.log(sid!=aman);
+// console.log(sid === aman);
+
+// console.log(typeof (sid))
+// console.log(typeof (aman))
+// // data type in javascript 
+// // number 1243
+
+// // stering  "sid" "24"
+
+// //  Boolean true false
+
+// // array[1, 3, 5, 6, 3, sid]
+
+// // BigInt 2435445463465466547547
+
+// // Symbol  unique values
+
+// // null  empty
+// // undefined not defined 
+// console.log(typeof(Number));
+// console.log(typeof(NaN));
+// console.log(typeof(undefined));
+// console.log(typeof(Boolean));
+// console.log(typeof(String));
+
+// // increment or decrement opretors 
+// // ++ ,--
+// var a = 30;
+// // a++; 
+//  a= a+1
+// console.log(a);
+// a--;
+// a= a-1;
+// console.log(a);
 
 
+//  conditions in javascript 
+
+// if else / switch case 
+
+// if(condition){ 
+//     -----
+// } else{ 
+
+// }
+
+// var  age = 18;
+
+// if (age>=18){ 
+//     console.log("you can vote");
+    
+// }else{ 
+//     console.log("you can not vote");
+    
+// }
 
 
+// var math = 45;
+// var che = 76;
+// var phy = 35;
+// var per = (math+che+phy)/3;
+// if(math>33&&che>33&&phy>33&&per>33){ 
+//     console.log("you are pass ");
+    
+// }else{ 
+//     console.log("you r fail ");
+    
+// }
 
+// var signal = "yellow";
+
+// if(signal=="red"){ 
+//     console.log("ruk ja bete..");
+// } else if(signal=="green"){ 
+//     console.log("chale ja bete..");
+    
+// }else{ 
+//     console.log("ruko jara sabar karo ");
+    
+// }
+// switch case in javascript 
+
+// var a =34;
+// var b = 22; 
+// var op = "/"
+// switch(op){ 
+//     case '+':
+//         console.log(a+b);
+//         break;
+//     case '-':
+//         console.log(a-b);
+//         break;
+//     case '*':
+//         console.log(a*b);
+//         break;
+//     case '/':
+//         console.log(a/b);
+//         break;
+       
+// }
+
+// console.log(2*1);
+// console.log(2*2);
+// console.log(2*3);
+// console.log(2*4);
+// console.log(2*5);
+// console.log(2*6);
+// console.log(2*7);
+// console.log(2*8);
+// console.log(2*9);
+// console.log(2*10);
+
+var n = 1000;
+for(var i = 1; i<=n;i++){ 
+    console.log("baby sorry ");
+    console.log("maanjao");
+    
+    
+}
---
+++ object/index.js
@@ -0,0 +1,19 @@
+// console.log("hello from the obj js file");
+//  object
+// var car_color = "red"
+// var car_brand = "mahindra"
+// var car_type = "xuv"
+var car  = { 
+    name : "thar" ,
+    color : "black" ,
+    brand : "mahinadra",
+    type : "xuv",
+    tegline : function teg(){ 
+        console.log("this is thar 4*4 very power full car ");
+        
+    }
+}
+console.log(car.name);
+console.log(car.brand);
+console.log(car.type);
+console.log(car.tegline());
