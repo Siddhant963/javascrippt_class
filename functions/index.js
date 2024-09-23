b9baa6738fcf8009f74f36c6ef31b1561a796c96
Siddhantwork099 <siddhantwork867@gmail.com>
2024-09-24 02:20:23 +0530

    building function

---
+++ functions/index.js
@@ -0,0 +1,27 @@
+// function saythanks ()   // function defe
+// { 
+//     console.log("thank you!");  //fun body
+    
+// }
+
+//    //fun call
+
+// for(var i = 1 ;i<= 100; i++){ 
+//     saythanks();
+//     console.log(" ");
+    
+// }
+
+
+function fec(num){ 
+    var fecto = 1;
+    for( var i = 1; i<=num ;i++){ 
+        fecto = fecto*i;
+    }
+  console.log(fecto);
+  
+}
+ 
+for( var i =5 ;i<=10;i++){ 
+    fec(i);
+}
--- object/index.js
+++ object/index.js
@@ -3,17 +3,48 @@
 // var car_color = "red"
 // var car_brand = "mahindra"
 // var car_type = "xuv"
-var car  = { 
-    name : "thar" ,
-    color : "black" ,
-    brand : "mahinadra",
-    type : "xuv",
-    tegline : function teg(){ 
-        console.log("this is thar 4*4 very power full car ");
+// var car  = { 
+//     name : "thar" ,
+//     color : "black" ,
+//     brand : "mahinadra",
+//     type : "xuv",
+//     tegline : function teg(){ 
+//         console.log("this is thar 4*4 very power full car ");
         
+//     }
+// }
+
+console.log('object in advance');
+
+// var student = { 
+//     name: "siddhant",
+//     class : "10th",
+//     strem : "maths"
+// }
+
+
+// var student2 = { 
+//     name: "aman",
+//     class : "11th",
+//     strem : "maths"
+// }
+
+// var student = { 
+//     name: "siddhant",
+//     class : "10th",
+//     strem : "maths"
+// }
+ function createStudent(name , classs , strem){ 
+    return{ 
+        name: name,
+        class : classs,
+        strem : strem
     }
-}
-console.log(car.name);
-console.log(car.brand);
-console.log(car.type);
-console.log(car.tegline());
+ }
+
+
+var student1 = createStudent( "siddhant" , 11 , "bio" );
+console.log(student1);
+var student2 = createStudent("harsh " , 9 , 'allsub')
+console.log(student2);
+
