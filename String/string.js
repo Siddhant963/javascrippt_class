1f4bb6140a390664e2c6e9596c7e19f7c5e62911
Siddhantwork099 <siddhantwork867@gmail.com>
2024-09-28 21:11:44 +0530

    string

---
+++ String/string.js
@@ -0,0 +1,41 @@
+console.log("String in js ");
+
+// "" ''   ``  with the help of these symbol we can create a string in js 
+
+// var string = "hello youtube family ";
+// var name = "hello my name is siddhant and my father's name is mr anil dubey ji ..."
+// var address = `jabalpur
+//  is a good and 
+//  clean city`
+// console.log(string);
+// console.log(name);
+// console.log(address);
+var string = "hello a youtube family     ";
+var name = "hello my name is siddhant and my father's name is mr anil dubey ji ..."
+var address = `jabalpur 
+ is a good and 
+ clean city`
+
+ console.log(string.length);
+ console.log(typeof address);
+ console.log(address.charAt(6));
+ console.log(address.charCodeAt(0));
+ console.log(address.includes("good"));
+ console.log(address.split(" "));
+ console.log(string.concat(",how are you"));
+ console.log(string.endsWith("iy"));
+ console.log(string.indexOf("a"));
+ console.log(string.lastIndexOf("a"));
+ console.log(string.toUpperCase());
+
+
+ 
+ 
+ 
+
+
+
+ 
+
+
+
